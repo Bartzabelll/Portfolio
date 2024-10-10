85c26160a72e3e79dae5d1b7a0af2d058143995c
Jhenrich <85864804+Bartzabell@users.noreply.github.com>
2024-10-10 08:10:40 +0800

    update

---
+++ tailwind-config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'arial': ['Arial', 'sans-serif'],
+                'roboto': ['Roboto', 'sans-serif'],
+                'engravers': ['"Engravers MT"', 'sans-serif'], // Add Engravers MT
+            }
+        }
+    }
+};
