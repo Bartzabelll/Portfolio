5848012e36971e0b818796880ec2bd01c93ae7c8
Jhenrich <140937845+Bartzabelll@users.noreply.github.com>
2024-10-10 13:45:02 +0800

    cleaner

--- tailwind-config.js
+++ tailwind-config.js
@@ -2,13 +2,13 @@ tailwind.config = {
     theme: {
         extend: {
             height: {
-                '128': '30rem', // Example of height larger than h-96 (which is 24rem)
+                '128': '30rem', 
                 '144': '36rem',
             },
             fontFamily: {
                 'arial': ['Arial', 'sans-serif'],
                 'roboto': ['Roboto', 'sans-serif'],
-                'engravers': ['"Engravers MT"', 'sans-serif'], // Add Engravers MT
+                'engravers': ['"Engravers MT"', 'sans-serif'], 
             }
         }
     }
