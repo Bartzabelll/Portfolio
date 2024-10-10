341eed2bc3895fa6f8d7abfc4ff9f1d1616efcca
Jhenrich <140937845+Bartzabelll@users.noreply.github.com>
2024-10-10 11:06:25 +0800

    push

--- tailwind-config.js
+++ tailwind-config.js
@@ -1,6 +1,10 @@
 tailwind.config = {
     theme: {
         extend: {
+            height: {
+                '128': '30rem', // Example of height larger than h-96 (which is 24rem)
+                '144': '36rem',
+            },
             fontFamily: {
                 'arial': ['Arial', 'sans-serif'],
                 'roboto': ['Roboto', 'sans-serif'],
